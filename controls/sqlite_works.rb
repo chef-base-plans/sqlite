5b07159fcb909c7554ba0a6b13cb841bb8f876eb
Steven Marshall <SMarshall@chef.io>
2020-06-23 13:57:54 +0100

    [sqlite] Various refactors of existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest binary format
    Added plan_name to attributes.yml
    Updates controls to add more prescriptive version checking.
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/sqlite_works.rb
+++ controls/sqlite_works.rb
@@ -9,29 +9,34 @@ control 'core-plans-sqlite' do
   impact 1.0
   title 'Ensure sqlite binary is working as expected'
   desc '
-We first check that the sqlite binary we expect is present and then run a version check to verify that it is excecutable.
+  To test the sqlite3 binary that core/sqlite provides we first check for the installation directory.
+  Using this directory we then run checks to ensure the binary exists.
+  Then we test that the version of the binary we expect to be installed exists.
+    $ $PKG_PATH/bin/sqlite3 --version
+      3.31.1 2020-01-27 19:55:54 3bfa9cc97da10598521b342961df8f5f68c7388fa117345eeb516eaa837bb4d6
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stderr') { should be_empty}
+    its('exit_status') { should eq 0 }
   end
 
   target_dir = File.join(hab_pkg_path.stdout.strip, base_dir)
 
-  sqlite_exists = command("ls #{File.join(target_dir, "sqlite3")}")
+  sqlite_exists = command("ls -al #{File.join(target_dir, "sqlite3")}")
   describe sqlite_exists do
     its('stdout') { should match /sqlite3/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  sqlite_works = command("/bin/sqlite3 --version")
+  sqlite_works = command("#{File.join(target_dir, "sqlite3")} --version")
   describe sqlite_works do
-    its('stdout') { should match /[0-9]+.[0-9]+.[0-9]+/ }
+    its('stdout') { should match /#{hab_pkg_path.stdout.strip.split('/')[5]}/ }
     its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
+
 end
