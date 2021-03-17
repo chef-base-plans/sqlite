a3b5e289bf29c0d01ce2bcfcebe8ae940174e8eb
Lucas Giancola <lucas.giancola@live.com>
2021-03-17 14:16:15 -0400

    Bumped version 3.31.1 -> 3.35.1

--- plan.sh
+++ plan.sh
@@ -1,15 +1,15 @@
 pkg_name=sqlite
-pkg_version=3.31.1
-pkg_dist_version=3310100
+pkg_version=3.35.1
+pkg_dist_version=3350100
 pkg_origin=core
 pkg_license=('Public Domain')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine."
 pkg_upstream_url=https://www.sqlite.org/
-pkg_source="https://www.sqlite.org/2020/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
+pkg_source="https://www.sqlite.org/2021/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_filename="${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_dirname="${pkg_name}-autoconf-${pkg_dist_version}"
-pkg_shasum=62284efebc05a76f909c580ffa5c008a7d22a1287285d68b7825a2b6b51949ae
+pkg_shasum=dce5616c059084887ccf64739e368c161c49c64e7a7e43a47c2940d22d29560f
 pkg_deps=(
   core/glibc
   core/readline
